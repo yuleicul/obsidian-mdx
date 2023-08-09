3c60b88e952bf12b069f8fb70e1ebe8c85a84e03
Yulei Chen <yulei_chen@epam.com>
2023-08-09 23:28:12 +0800

    #1 - Add command MDX:Preview

---
+++ src/main.ts
@@ -0,0 +1,37 @@
+import { Editor, MarkdownView, Plugin } from 'obsidian'
+import { MDXPreviewState, MDX_PREVIEW, mdxPreview } from './mdxPreview'
+
+export default class ObsidianKetcher extends Plugin {
+  async onload() {
+    this.registerView(MDX_PREVIEW, (leaf) => new mdxPreview(leaf))
+
+    this.addCommand({
+      id: 'mdx-preview',
+      name: 'Preview',
+      editorCheckCallback: (
+        checking: boolean,
+        _editor: Editor,
+        view: MarkdownView
+      ) => {
+        if (['mdx', 'md'].includes(view.file.extension)) {
+          if (!checking) {
+            this.app.workspace.detachLeavesOfType(MDX_PREVIEW)
+            const leaf = this.app.workspace.getLeaf('tab')
+            const viewState: MDXPreviewState = {
+              data: view.data,
+              basename: view.file.basename,
+            }
+            leaf.setViewState({
+              type: MDX_PREVIEW,
+              state: viewState,
+              active: true,
+            })
+            this.app.workspace.revealLeaf(leaf)
+          }
+          return true
+        }
+        return false
+      },
+    })
+  }
+}
